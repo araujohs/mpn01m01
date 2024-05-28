5e847640826993d40020bad0e4bf5f04d7a4a4ee
Helio Araujo <araujohs@gmail.com>
2024-05-28 15:28:09 -0300

    Create entrada.py
    Microatividade 5

---
+++ entrada.py
@@ -0,0 +1,9 @@
+print("Universidade Estácio")
+print("Curso: Desenvolvimento Full Stack")
+print("Período: 2024.1")
+
+print("Microatividade 5")
+
+name = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+print('Olá, ' + name + '. Você tem ' + idade + ' anos.')
