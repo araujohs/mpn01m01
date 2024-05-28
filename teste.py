b1634568d5d6d346e973c716d8cfe830fb4dc0c1
Helio Araujo <araujohs@gmail.com>
2024-05-28 15:22:45 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,15 @@
+print("Universidade Estácio")
+print("Curso: Desenvolvimento Full Stack")
+print("Período: 2024.1")
+
+print("Microatividade 3")
+
+
+print("Hello World")
+
+
+print("Dados os números 123 e 3 executar as 4 operações básicas com estes números")
+print("Soma     :     123 + 3 = ", 123 + 3)
+print("Subtração:     123 - 3 = ", 123 - 3)
+print("Multiplicação: 123 * 3 = ", 123 * 3)
+print("Divisão:       123 / 3 = ", 123 / 3)
