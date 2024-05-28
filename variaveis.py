675f4000cb7ef7dad339a7c728a6e8cbe87af6d1
Helio Araujo <araujohs@gmail.com>
2024-05-28 15:25:38 -0300

    Create variaveis.py
    Microatividade 4

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+print("Universidade Estácio")
+print("Curso: Desenvolvimento Full Stack")
+print("Período: 2024.1")
+
+print("Microatividade 4")
+
+a = 15
+b = 20
+soma = a + b
+print("Resultado da soma:          " + str(soma))
+
+#comandos adicionais ao programa original
+mult = a * b
+print("Resultado da multiplicação: " + str(mult))
+
+subtr = a - b
+print("Resultado da subtração:     " + str(subtr))
+
+divi = a / b
+print("Resultado da divisão:       " + str(divi))
